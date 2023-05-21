b542ac96df6f5b55a1eb163996452466d409f5f1
Nelson Rojas <nelson_rojas_n@hotmail.com>
2023-05-21 16:12:48 -0400

    Mejora en la clase modelo

--- Libs/Modelo.php
+++ Libs/Modelo.php
@@ -34,20 +34,6 @@ class Modelo
         }
     }
 
-    private function ejecutar_metodo_antes_de_accion($nombre_del_metodo)
-    {
-        if ((int) method_exists($this, $nombre_del_metodo)) {
-            call_user_func(array($this, $nombre_del_metodo));
-        }
-    }
-
-    private function ejecutar_metodo_despues_de_accion($nombre_del_metodo)
-    {
-        if ((int) method_exists($this, $nombre_del_metodo)) {
-            call_user_func(array($this, $nombre_del_metodo));
-        }
-    }
-
     public function asignarNombreDeTabla(string $nombre_de_tabla)
     {
         $this->_nombre_tabla = $nombre_de_tabla;
@@ -85,30 +71,32 @@ class Modelo
     
     public function agregar($datos)
     {
-        $this->ejecutar_metodo_antes_de_accion("antes_de_agregar");
+        $this->antes_de_agregar();
         return Bd::insertar($this->_nombre_tabla, $datos);
-        $this->ejecutar_metodo_despues_de_accion("despues_de_agregar");
+        $this->despues_de_agregar();
     }
 
     public function actualizar($datos, $condicion = null)
     {
-        $this->ejecutar_metodo_antes_de_accion("antes_de_actualizar");
+        $this->antes_de_actualizar();
         return Bd::actualizar($this->_nombre_tabla, $datos, $condicion);
-        $this->ejecutar_metodo_despues_de_accion("despues_de_actualizar");
+        $this->despues_de_actualizar();
     }
     
     public function eliminarTodos($condicion, $parametros = null)
     {
+        $this->antes_de_eliminar();
         return Bd::eliminar($this->_nombre_tabla, $condicion, $parametros);
+        $this->despues_de_eliminar();
     }
     
     public function eliminar()
     {
         if (intval($this->id) > 0) {
             $condicion = " WHERE id = " . intval($this->id);
-            $this->ejecutar_metodo_antes_de_accion("antes_de_eliminar");
+            $this->antes_de_eliminar();
             return Bd::eliminar($this->_nombre_tabla, $condicion);
-            $this->ejecutar_metodo_despues_de_accion("despues_de_eliminar");
+            $this->despues_de_eliminar();
         } else {
             return false;
         }
@@ -124,13 +112,13 @@ class Modelo
     public function guardar()
     {
         if (intval($this->id) > 0) {
-            $this->ejecutar_metodo_antes_de_accion("antes_de_actualizar");
+            $this->antes_de_actualizar();
             return $this->actualizar($this->_datos, " WHERE id = " . $this->id) > 0;
-            $this->ejecutar_metodo_despues_de_accion("despues_de_actualizar");
+            $this->despues_de_actualizar();
         } else {
-            $this->ejecutar_metodo_antes_de_accion("antes_de_agregar");
+            $this->antes_de_agregar();
             $nuevo_id = intval($this->agregar($this->_datos));
-            $this->ejecutar_metodo_despues_de_accion("despues_de_agregar");
+            $this->despues_de_agregar();
             $this->id = $nuevo_id;
             return $nuevo_id > 0;
         }
@@ -149,5 +137,10 @@ class Modelo
         return $this->_datos[$atributo] ?? null;
     }
     
-       
+    public function __call($nombre_del_metodo, $args)
+    {
+        if ((int) method_exists($this, $nombre_del_metodo)) {
+            call_user_func(array($this, $nombre_del_metodo));
+        }
+    }   
 }
