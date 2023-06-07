52e491852f2442283eb48ce6754fc2f7383b7d01
Nelson Rojas <nelson_rojas_n@hotmail.com>
2023-06-07 09:36:16 -0400

    Incluir Scaffolding

---
+++ App/Libs/AdminController.php
@@ -0,0 +1,13 @@
+<?php 
+
+class AdminController extends Controller
+{
+    /**
+     * puede servir para extender desde este controlador e incluir en él
+     * opciones de autentificación
+     */
+    public function beforeFilter()
+    {
+        
+    }
+}
