4b3d4efc49d64304cfed57d60e71ddceab466da3
Nelson Rojas <74690438+nelsonrojasn@users.noreply.github.com>
2024-11-03 12:01:02 -0300

    agregar cargador de IsaliaPHP
    Se agregar un cargador para contar con acceso a las funcionalidades desde al consola interactiva de PHP

---
+++ app/bin/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+date_default_timezone_set("America/Santiago");
+
+define('ROOT', dirname(dirname(__DIR__)));
+define('DS', DIRECTORY_SEPARATOR);
+define('APP_PATH', ROOT . DS . 'app' . DS);
+define('CORE_PATH', ROOT . DS . 'core' . DS);
+define('LIBS_PATH', CORE_PATH . 'libs' . DS);
+define('PUBLIC_PATH', '/');
+define('START_TIME', microtime(true));
+
+/**
+ * Generación de carga automática de clases
+ */
+require_once LIBS_PATH . "autoloader.php";
+
+spl_autoload_register(
+    function ($class_name) {
+        Autoloader::exec($class_name);
+    }
+);
+/**
+ * Asignamos los manejadores de errores y excepciones
+ */
+set_error_handler('Report::handleError');
+set_exception_handler('Report::handleException');
